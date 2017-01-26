7c18f12edd0619efc9c128f14143856775b35120
Marc Tannous <tannous.marc@gmail.com>
2017-01-26 18:31:20 +0000

    taking the cascading out of CSS fix

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+import styles from './app.css'
+
+class App extends Component{
+  render() {
+    return <h1 className={styles.heading}>Hello Worlds!</h1>
+  }
+}
+
+export default App
